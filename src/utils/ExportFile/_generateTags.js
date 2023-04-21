de067212766d57477a788c5527a7f429eb3054c7
Mihai-Cristian Bâltac <baltacmihaicristian@gmail.com>
2023-04-21 16:36:07 +0300

    generate attributes

--- src/utils/ExportFile/_generateTags.js
+++ src/utils/ExportFile/_generateTags.js
@@ -3,14 +3,43 @@ export default function generateTags(tag, depth = 0) {
 
   switch (tag.tagType) {
     case "selfClose":
-      return `${indent}<${tag.tagName} ${tag.tagAttributes}/>\n`;
+      return `${indent}<${tag.tagName} ${generateTagAttributesString(
+        tag.tagAttributes
+      )}/>\n`;
 
     case "text":
-      return `${indent}<${tag.tagName} ${tag.tagAttributes}> ${tag.content}</${tag.tagName}>\n`;
+      return `${indent}<${tag.tagName} ${generateTagAttributesString(
+        tag.tagAttributes
+      )}> ${tag.content}</${tag.tagName}>\n`;
+
+    case "media":
+      return `${indent}<${tag.tagName} ${generateTagAttributesString(
+        tag.tagAttributes
+      )}> ${tag.content}</${tag.tagName}>\n`;
+
+    case "link":
+      return `${indent}<${tag.tagName} ${generateTagAttributesString(
+        tag.tagAttributes
+      )}> ${tag.content}</${tag.tagName}>\n`;
 
     case "container":
-      return `${indent}<${tag.tagName} ${tag.tagAttributes}> \n${tag.content
+      return `${indent}<${tag.tagName} ${generateTagAttributesString(
+        tag.tagAttributes
+      )}> \n${tag.content
         ?.map((tagChild) => generateTags(tagChild, depth + 2))
         .join("")}\n${indent} </${tag.tagName}> \n`;
   }
+}
+
+function generateTagAttributesString(tagAttributes) {
+  const tagAttributesList = [];
+  for (const [key, value] of Object.entries(tagAttributes)) {
+    if (value && key != "custom") {
+      tagAttributesList.push(`${key}="${value}"`);
+    }
+  }
+  if (tagAttributes.custom) {
+    tagAttributesList.push(tagAttributes.custom);
+  }
+  return tagAttributesList.join(" ");
 }
